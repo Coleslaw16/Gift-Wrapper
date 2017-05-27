d889c8715a38a9da442f44b2d7a4ad2ea8c2161c
Coleslaw16 <jstone18@murraystate.edu>
2017-05-27 00:32:42 -0400

    Add files via upload

---
+++ TakeHomeTestProblem2/TakeHomeTestProblem2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TakeHomeTestProblem2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.getFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lineBox = new System.Windows.Forms.TextBox();
+            this.drawLine = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getFileToolStripMenuItem,
+            this.saveFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(872, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // getFileToolStripMenuItem
+            // 
+            this.getFileToolStripMenuItem.Name = "getFileToolStripMenuItem";
+            this.getFileToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.getFileToolStripMenuItem.Text = "Get File";
+            this.getFileToolStripMenuItem.Click += new System.EventHandler(this.getFileToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.saveFileToolStripMenuItem.Text = "Save File";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(138, 2);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(54, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "1";
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Scale Factor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Dot Size";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lineBox
+            // 
+            this.lineBox.Location = new System.Drawing.Point(465, 4);
+            this.lineBox.Name = "lineBox";
+            this.lineBox.Size = new System.Drawing.Size(48, 20);
+            this.lineBox.TabIndex = 5;
+            this.lineBox.Text = "2";
+            this.lineBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // drawLine
+            // 
+            this.drawLine.Location = new System.Drawing.Point(519, 1);
+            this.drawLine.Name = "drawLine";
+            this.drawLine.Size = new System.Drawing.Size(75, 23);
+            this.drawLine.TabIndex = 6;
+            this.drawLine.Text = "Line Size";
+            this.drawLine.UseVisualStyleBackColor = true;
+            this.drawLine.Click += new System.EventHandler(this.drawLine_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(690, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Concentric Puzzle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.White;
+            this.picBox.Location = new System.Drawing.Point(0, 28);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(872, 556);
+            this.picBox.TabIndex = 9;
+            this.picBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 581);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.drawLine);
+            this.Controls.Add(this.lineBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem getFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox lineBox;
+        private System.Windows.Forms.Button drawLine;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ TakeHomeTestProblem2/TakeHomeTestProblem2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Collections;
+
+namespace TakeHomeTestProblem2
+{
+    public partial class Form1 : Form
+    {
+        string filename = "";
+        private points[] pointLocation;
+        private ArrayList tempForConvex;
+        private Bitmap aBitMap;
+        private float scale;
+        private float dotSize;
+        private float lineSize;
+        private Color Color = Color.Black;
+        private bool drawingLines = false;
+        private string output ="";
+        public Form1()
+        {
+            InitializeComponent();
+            ResizeRedraw = true;
+            scale = float.Parse(textBox.Text);
+            dotSize = float.Parse(textBox1.Text);
+            lineSize = float.Parse(lineBox.Text);
+        }
+
+        private void getFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                filename = dlg.FileName;
+                readFile();
+            }
+        }
+        private void readFile()
+        {
+            try
+            {
+                StreamReader st = new StreamReader(filename);
+                int arraySize = int.Parse(st.ReadLine());
+                Console.WriteLine(arraySize);
+                pointLocation = new points[arraySize];
+                for (int i = 0; i < arraySize; i++)
+                {
+                    string[] temp = st.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                    int x = int.Parse(temp[0]);
+                    int y = int.Parse(temp[1]);
+                    pointLocation[i] = (new points(x, y, dotSize, Color));
+                }
+                st.Close();
+                tempForConvex = new ArrayList();
+                foreach(points x in pointLocation)
+                {
+                    tempForConvex.Add(x);
+                }
+                drawPoints();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("You messed up somewhere");
+            }
+        }
+
+        private void resetFrameBuffer()
+        {
+            Graphics g = CreateGraphics();
+            if (aBitMap != null)
+                aBitMap.Dispose();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            aBitMap = new Bitmap(picBox.Width, picBox.Height, g);
+        }
+        private void drawPoints()
+        {
+            resetFrameBuffer();
+            Graphics g = Graphics.FromImage(aBitMap);
+            System.Drawing.SolidBrush aBrsh = new System.Drawing.SolidBrush(Color.Black);
+            if (drawingLines == true)
+            {
+                Pen drawingPen = new Pen(Brushes.Crimson, lineSize);
+                for (int i = 0; i < pointLocation.Length - 1; i++)
+                {
+                    g.DrawLine(drawingPen, pointLocation[i].X * scale, pointLocation[i].Y * scale, pointLocation[i + 1].X * scale, pointLocation[i + 1].Y * scale);
+                }
+                drawingPen.Dispose();
+            }
+            for (int i = 0; i < pointLocation.GetLength(0); i++)
+            {
+                g.FillEllipse(aBrsh, (float)(pointLocation[i].X * scale) - dotSize / 2.0F, (float)(pointLocation[i].Y * scale) - dotSize / 2.0F, dotSize, dotSize);
+            }
+            picBox.Image = aBitMap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scale = float.Parse(textBox.Text);
+            drawPoints();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dotSize = float.Parse(textBox1.Text);
+            drawPoints();
+        }
+
+        private void drawLine_Click(object sender, EventArgs e)
+        {
+            lineSize = float.Parse(lineBox.Text);
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFile.FilterIndex = 1;
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter w = new StreamWriter(saveFile.OpenFile());
+                w.Write(output);
+                w.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+ //           resetFrameBuffer();
+            if(pointLocation.Length<3)
+            {
+                throw new ArgumentException("You need at least 3 points");
+            }
+            while (tempForConvex.Count >= 3)
+            {
+                points min = (points)tempForConvex[0];
+                foreach (points x in tempForConvex)
+                {
+                    if (x.X < min.X)
+                    {
+                        min = x;
+                    }
+                }
+
+                List<points> hull = new List<points>();
+                points vEndpoint;
+                points vpointOnHull = min;
+                do
+                {
+                    hull.Add(vpointOnHull);
+                    vEndpoint = (points)tempForConvex[0];
+
+                    for (int j = 1; j < tempForConvex.Count; j++)
+                    {
+                        if (vpointOnHull == vEndpoint || Orientation(vpointOnHull, vEndpoint, (points)tempForConvex[j]) == -1)
+                        {
+                            vEndpoint = (points)tempForConvex[j];
+                        }
+                    }
+                    vpointOnHull = vEndpoint;
+                } while (vEndpoint != hull[0]);
+
+                Pen drawingPen = new Pen(Brushes.Crimson, lineSize);
+                points[] tempCH = new points[hull.Count];
+                int count = 0;
+                foreach (points x in hull)
+                {
+                    tempCH[count] = x;
+                    count++;
+                    output += x.X + " " + x.Y + "\r\n";
+                }
+                foreach(points x in hull)
+                {
+                    tempForConvex.Remove(x);
+                }
+                Graphics g = Graphics.FromImage(aBitMap);
+                System.Drawing.SolidBrush aBrsh = new System.Drawing.SolidBrush(Color.Black);
+                for (int i = 0; i < tempCH.Length - 1; i++)
+                {
+                    g.DrawLine(drawingPen, tempCH[i].X * scale, tempCH[i].Y * scale, tempCH[i + 1].X * scale, tempCH[i + 1].Y * scale);
+                }
+                g.DrawLine(drawingPen, tempCH[tempCH.Length - 1].X * scale, tempCH[tempCH.Length - 1].Y * scale, tempCH[0].X * scale, tempCH[0].Y * scale);
+                drawingPen.Dispose();
+                for (int i = 0; i < pointLocation.GetLength(0); i++)
+                {
+                    g.FillEllipse(aBrsh, (float)(pointLocation[i].X * scale) - dotSize / 2.0F, (float)(pointLocation[i].Y * scale) - dotSize / 2.0F, dotSize, dotSize);
+                }
+                output += "\r\n";
+            }
+            picBox.Image = aBitMap;
+        }
+
+        private int Orientation(points p1, points p2, points p)
+        {
+            int orin = (int)((p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y));
+
+            if (orin > 0)
+                return -1;
+            if (orin < 0)
+                return 1;
+
+            return 0;
+        }
+    }
+}
